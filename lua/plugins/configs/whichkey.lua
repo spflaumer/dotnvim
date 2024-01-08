e69a7a9cd94df3ef0c12a5986f665dba26b90067
spflaumer <s.pflaumer@murena.io>
2024-01-08 12:18:13 +0100

    optimize which-key and add noice external config

---
+++ lua/plugins/configs/whichkey.lua
@@ -0,0 +1,13 @@
+return {
+    plugins = {
+        marks = true,
+        registers = true,
+        spelling = { enabled = true, suggestions = 10 },
+    },
+    key_labels = {
+        ["<leader>"] = "LDR",
+        ["<space>"] = "SPC",
+        ["<cr>"] = "RET",
+        ["<tab>"] = "TAB",
+    }
+}
